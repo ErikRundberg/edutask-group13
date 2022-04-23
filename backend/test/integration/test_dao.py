b265de36f0dfbd2da60aa607144c32b0d90157cb
Varg01 <patrik.karlsson.03@hotmail.com>
2022-04-23 11:56:28 +0200

    Added type test

--- backend/test/integration/test_dao.py
+++ backend/test/integration/test_dao.py
@@ -19,39 +19,25 @@ import json
 from bson import json_util
 from bson.objectid import ObjectId
 
-# # load the local mongo URL (something like mongodb://localhost:27017)
-#         LOCAL_MONGO_URL = dotenv_values('.env').get('MONGO_URL')
-#         # check out of the environment (which can be overridden by the docker-compose file) also specifies an URL, and use that instead if it exists
-#         MONGO_URL = os.environ.get('MONGO_URL', LOCAL_MONGO_URL)
-
-#         client = pymongo.MongoClient(MONGO_URL)
-#         client.drop_database("testedutask")
-#         database = client.testedutask
-
-#         validator = getValidator("user")
-#         database.create_collection("user", validator=validator)
-# client.drop_database("testedutask")
 
 #pytest -m namespaces 
 class TestDAO:
     @pytest.fixture
     def sut(self):
-        LOCAL_MONGO_URL = dotenv_values('.env').get('MONGO_URL')
-        MONGO_URL = os.environ.get('MONGO_URL', LOCAL_MONGO_URL)
-        client = pymongo.MongoClient(MONGO_URL)
+        with open(f'./test/static/data/test.json', 'r') as f:
+            validator = json.load(f)
+        with patch('src.util.dao.getValidator', return_value=validator, autospec=True):
+            dao = DAO("test")
 
-        sut = DAO("test_table")
-        with patch('src.util.helpers.DAO.getValidator', autospec=True) as sut.mocked_validator:
-            with open(f'./test/static/data/{"test.json"}.json', 'r') as f:
-                sut.mocked_validator = json.load(f)
-            yield sut
-            client.drop("test_table")
+            yield dao
+            dao.drop()
 
-    def test_create(self):
-        to_insert = [{
+    def test_create(self, sut):
+        to_insert = {
             "email": "pakr20@student.bth.se",
             "firstName": "Patrik",
-            "lastName": "Karlsson"
-        }]
+            "lastName": "Karlsson",
+            "ObjecId": "626117e2e52221c92c411ae4"
+        }
         # File patch in validator
-        assert DAO.create(self, to_insert) == to_insert
+        assert sut.create(to_insert) == to_insert
