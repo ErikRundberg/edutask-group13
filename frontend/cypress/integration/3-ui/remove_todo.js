bad58b5c8d4f20b57d7bb44d6b7dffb5e6e6089e
Patrik Karlsson <patrik.karlsson.03@hotmail.com>
2022-05-08 12:53:53 +0200

    Added two new test files

---
+++ frontend/cypress/integration/3-ui/remove_todo.js
@@ -0,0 +1,80 @@
+/// <reference types="cypress" />
+
+describe('Logging into the system', () => {
+    var secondTest = false;
+    beforeEach(function() {
+        // create a fabricated user from a fixture
+        cy.visit("http://localhost:3000/")
+        cy.fixture('user.json')
+        .then((user) => {
+            cy.request({
+                method: 'POST',
+                url: 'http://localhost:5000/users/create',
+                    form: true,
+                    body: user
+                }).then((response) => {
+                    this.uid = response.body._id.$oid
+
+                    // create one fabricated task for that user
+                    cy.fixture('task.json')
+                    .then((task) => {
+                            // add the user id to the data of the task object
+                            task.userid = this.uid
+                            cy.request({
+                                method: 'POST',
+                                url: 'http://localhost:5000/tasks/create',
+                                form: true,
+                                body: task
+                            }).then((response) => {
+                                this.tid = response.body[0]._id.$oid
+
+                                cy.fixture('todo.json').then((todo) => {
+                                    // add the task id to the data of the todo object
+                                    todo.taskid = this.tid
+                                    cy.request({
+                                        method: 'POST',
+                                        url: 'http://localhost:5000/todos/create',
+                                        form: true,
+                                        body: todo
+                                    }).then((response) => {
+                                        this.toid = response.body
+                                    })
+                                })
+                            })
+                        })
+                })
+            })
+            cy.contains('div', 'Email Address')
+            .find('input[type=text]')
+            .type('mon.doe@gmail.com')
+            // alternative, imperative way of detecting that input field
+            /*cy.get('.inputwrapper #email')
+                .type('mon.doe@gmail.com')*/
+
+            // submit the form on this page
+            cy.get('form')
+                .submit()
+
+            cy.get('img')
+                .click()
+
+    })
+
+
+    it('remove todo', () => {
+        // find span and click it to remove
+        cy.get('.todo-list').find('li').eq(1).find('span').last().click()
+        cy.get('.todo-list').find('li').eq(1).should("not.exist")
+    })
+
+
+    afterEach(function() {
+        // clean up by deleting the user from the database
+        cy.request({
+            method: 'DELETE',
+            url: `http://localhost:5000/users/${this.uid}`
+        }).then((response) => {
+            cy.log(response.body)
+        })
+    })
+})
